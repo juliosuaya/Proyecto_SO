13ed49209154a04b0f99c8a7d336cd235b41886b
Gonzalo <gro1992@hotmail.com>
2021-05-30 14:54:20 -0300

    Comentarios

--- Proyecto/src/com/Main.java
+++ Proyecto/src/com/Main.java
@@ -9,19 +9,19 @@ public class Main {
 
 
         ListaEspera listaDeEspera= new ListaEspera();
-        Recursos[] recursos = {
+        Recursos[] recursos = { // Se crea lista de recursos
                 new Recursos("Española","Juan carlos","Sinovac", listaDeEspera),
                 new Recursos("Hospital de Clinicas","Pedro Martinez","Pfizer", listaDeEspera),
                 new Recursos("Britanico","Jose Martinez","Pfizer", listaDeEspera)};
-        Agenda agenda1 = new Agenda(listaDeEspera);
-        Reloj rel = new Reloj(recursos, listaDeEspera, agenda1);
+        Agenda agenda1 = new Agenda(listaDeEspera); //Se instancia agenda con una lista de espera
+        Reloj rel = new Reloj(recursos, listaDeEspera, agenda1); // Se instancia Reloj con los recursos, la lista de espera y la agenda
 
-        listaDeEspera.start();
-        agenda1.start();
-        for (Recursos recurso : recursos) {
+        listaDeEspera.start(); //Corro el hilo de lista de espera
+        agenda1.start(); // Corro el hilo de Agenda
+        for (Recursos recurso : recursos) { // Corro los hilos de recursos
             recurso.start();
         }
-        rel.start();
+        rel.start(); // Corro hilo del reloj
     }
     public static void imprimir(String s){
         try {
